ab50a04557a50b11276a647bd7671b4ea7554c6c
Ricardo Nainggolan <skatashock@gmail.com>
2019-07-24 16:39:21 +0700

    add validation

--- pyfop.py
+++ pyfop.py
@@ -10,11 +10,6 @@ import bisect
 # Context=internal-mas
 # icon=1
 
-# user's input for extension, label & context
-input_extension = input('Extension number: ').strip()
-input_label = input('Label: ').strip()
-input_context = input('Context: ').strip()
-
 # regex pattern
 sip_regex = re.compile(r'''(
     ((;?)\[SIP/(\d{2,4})\])\n   # 1: line, 2: commented, 3: ext. number
@@ -56,20 +51,57 @@ for result in results:
         # add above list to sips dictionary
         sips_data[extension] = extension_data
 
+# get input for Extension Number & some validation
+while True:
+    try:
+        # make sure the input is an integer
+        input_extension = int(input('Extension number: ').strip())
+        # check if the input number already exists
+        if str(input_extension) not in exts:
+            break
+        else:
+            print('The Extension number exists. Try again ...')
+    except ValueError:
+        print('Oops!  That was not a valid number.  Try again...')
+
+# get input for Label & some validation
+while True:
+    input_label = input('Label: ').strip()
+    # make sure input is not empty
+    if input_label:
+        break
+    else:
+        print('Please enter a Label value. Try again ...')
+
+# get input for Context & some validation
+while True:
+    input_context = input('Context: ').strip()
+    # make sure input is not empty
+    if input_label:
+        break
+    else:
+        print('Please enter a Context value. Try again ...')
+
 # get last extension
 exts_last = int(exts[-1])
 
+# get sip last position value, convert to int
+exts_last_position = int(sips_data[str(exts_last)][1])
+
+# add +1 to exts_new last position value
+exts_new_last_position = exts_last_position + 1
+
+# copy old exts to exts_new
+# we need before and after exts later
+exts_new = exts.copy()
+
+# insert user's input extension to exts_new in sorted order
+bisect.insort_left(exts_new, str(input_extension))
+
 # if input extension less than currently last extension
-if exts_last > int(input_extension):
-    # copy old exts to exts_new
-    # we need before and after exts later
-    exts_new = exts.copy()
-
-    # insert user's input extension to exts_new in sorted order
-    bisect.insort_left(exts_new, input_extension)
-
+if exts_last > input_extension:
     # find input_extension index number in exts_new list
-    input_extension_index = exts_new.index(input_extension)
+    input_extension_index = exts_new.index(str(input_extension))
 
     # next sip positions after inserted
     next_positions = []
@@ -79,41 +111,39 @@ if exts_last > int(input_extension):
         # add sip position to next_positions list
         next_positions.append(sips_data[exts[i]][1])
 
-    # get sip last position value, convert to int
-    exts_last_position = int(next_positions[len(next_positions) - 1])
-    # add +1 to exts_new last position value
-    exts_new_last_position = exts_last_position + 1
     # add to next_positions list, convert back to string
     next_positions.append(str(exts_new_last_position))
 
     # create list of new input data, including new position
     # then add to sips_data
-    input_data = [input_extension,
+    input_data = [str(input_extension),
                   next_positions[0],
                   input_label,
                   input_context]
-    sips_data[input_extension] = input_data
+
+    sips_data[str(input_extension)] = input_data
 
     # remove first index from next_positions
     next_positions.pop(0)
 
-    n = 0  # set initial n value
+    n = 0  # set initial n value to 0
 
     # iterate from after input_extension index to exts_new last index
     for i in range((input_extension_index + 1), len(exts_new)):
         # update all sips position after
         sips_data[exts_new[i]][1] = next_positions[n]
-        n += 1  # add n + 1
+        n += 1  # n = n + 1
 
 # else if input extension greater than currently last extension
-elif exts_last < int(input_extension):
-    exts_new = exts
-    # TODO: adjust position
-
-# input extension exists!
 else:
-    print('The extension number you entered already exist!')
-    sys.exit()
+    # create list of new input data, including new position
+    # then add to sips_data
+    input_data = [str(input_extension),
+                  exts_new_last_position,
+                  input_label,
+                  input_context]
+
+    sips_data[str(input_extension)] = input_data
 
 for ext in exts_new:
     print('[SIP/{0}]'.format(sips_data[ext][0]))
