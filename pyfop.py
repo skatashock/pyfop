8c2648d294be9e1e323a81cec4b7c59a55d7e3e4
Ricardo Nainggolan <skatashock@gmail.com>
2019-07-25 15:42:44 +0700

    code refactoring

--- pyfop.py
+++ pyfop.py
@@ -10,146 +10,203 @@ import bisect
 # Context=internal-mas
 # icon=1
 
-# regex pattern
-sip_regex = re.compile(r'''(
-    ((;?)\[SIP/(\d{2,4})\])\n   # 1: line, 2: commented, 3: ext. number
-    (Position=(\d{1,3}))\n      # 4: line, 5: position
-    (Label="(.*)")\n            # 6: line, 7: label
-    (Extension=(\d{2,4}))\n     # 8: line, 9: ext. number
-    (Context=(.*))\n            # 10: line, 11: context
-    (Icon=(\d+))\n              # 12: line, 13: icon
-    )''', re.IGNORECASE | re.VERBOSE)
+# Config filename
+config_filename = 'op_buttons.cfg'
 
-# open, copy content & close config file
-config_file = open('./op_buttons.cfg')
-config = config_file.read()
-config_file.close()
+# Extensions list, default to empty list
+exts = []
 
-# find all matching pattern from config file content
-results = sip_regex.findall(config)
-exts = []       # extension list variable
-positions = []  # position list variable
-sips_data = {}  # sips data dictionary variable
+# Extension numbers range between 2000 and 3999
+ext_start = 2000
+ext_end = 3999
 
-# iterate over each regex results
-for result in results:
-    # make sure it's not commented
-    if not result[2]:
-        # define variables
-        extension = result[3]   # extension
-        position = result[5]    # position
-        label = result[7]       # label
-        context = result[11]    # context
 
-        # add extension to extension list
-        exts.append(extension)
-        # add position to position list
-        positions.append(position)
+# Open, copy content and close config file
+def read_config(filepath):
+    config_file = open(filepath)
+    config = config_file.read()
+    config_file.close()
+    return config
 
-        # create list of data needed per extension
-        extension_data = [extension, position, label, context]
-        # add above list to sips dictionary
-        sips_data[extension] = extension_data
 
-# get input for Extension Number & some validation
-while True:
+# Find all sips function
+def sip_findall():
+    # Sips data dictionary
+    sips_data = {}
+
+    # Regex pattern
+    sip_regex = re.compile(r'''(
+        ((;?)\[SIP/(2\d{3}|3[0-2]\d{2})\])\n # 1:line, 3:ext. number
+        (Position=(\d{1,3}))\n      # 4:line, 5:position
+        (Label="(.*)")\n            # 6:line, 7:label
+        (Extension=(\d{2,4}))\n     # 8:line, 9:ext. number
+        (Context=(.*))\n            # 10:line, 11:context
+        (Icon=(\d+))\n              # 12:line, 13:icon
+        )''', re.IGNORECASE | re.VERBOSE)
+
+    # Get config file content
+    config = read_config(config_filename)
+    # Find all matching pattern
+    results = sip_regex.findall(config)
+
+    # Iterate over each regex results
+    for result in results:
+        # Make sure it's not commented
+        if not result[2]:
+            # Define variables
+            extension = result[3]   # extension
+            position = result[5]    # position
+            label = result[7]       # label
+            context = result[11]    # context
+
+            # Add extension to extension list
+            exts.append(extension)
+
+            # Create list of data needed per extension
+            extension_data = [extension, position, label, context]
+            # Add above list to sips dictionary
+            sips_data[extension] = extension_data
+
+    return sips_data
+
+# Write/update sips data to config file
+
+
+def write_config(exts_new, sips_data):
+    with open('test.cfg', 'w') as testfile:
+        for ext in exts_new:
+            testfile.write('[SIP/{0}]\n'.format(sips_data[ext][0]))
+            testfile.write('Position={0}\n'.format(sips_data[ext][1]))
+            testfile.write('Label=\"{0}\"\n'.format(sips_data[ext][2]))
+            testfile.write('Extension={0}\n'.format(sips_data[ext][0]))
+            testfile.write('Context={0}\n'.format(sips_data[ext][3]))
+            testfile.write('icon=1\n\n')
+
+
+# Check action and extension number argument/parameter
+if len(sys.argv) == 3:
+    # Make sure extension number is integer
     try:
-        # make sure the input is an integer
-        input_extension = int(input('Extension number: ').strip())
-        # check if the input number already exists
-        if str(input_extension) not in exts:
-            break
-        else:
-            print('The Extension number exists. Try again ...')
+        input_extension = int(sys.argv[2])
     except ValueError:
-        print('Oops!  That was not a valid number.  Try again...')
+        print('Oops! That was not a valid extension number. Exiting.')
+        sys.exit()
 
-# get input for Label & some validation
-while True:
-    input_label = input('Label: ').strip()
-    # make sure input is not empty
-    if input_label:
-        break
+    # Limit extension number between 2000 and 3999
+    if input_extension < ext_start or input_extension > ext_end:
+        print('Extension number must be between 2000 and 3999. Exiting.')
+        sys.exit()
+
+    # Add new extension
+    if sys.argv[1] == 'add':
+        # Get sips data
+        sips_data = sip_findall()
+
+        # Check if the input number already exists
+        if str(input_extension) in exts:
+            print('That extension number exists. Exiting.')
+            sys.exit()
+
+        # Get input for Label & some validation
+        while True:
+            input_label = input('Label: ').strip()
+            # Make sure input is not empty
+            if input_label:
+                break
+            else:
+                print('Please enter a Label value. Try again ...')
+
+        # Get input for Context & some validation
+        while True:
+            input_context = input('Context: ').strip()
+            # Make sure input is not empty
+            if input_context:
+                break
+            else:
+                print('Please enter a Context value. Try again ...')
+
+        # Get last extension
+        exts_last = int(exts[-1])
+
+        # Get sip last position value, convert to int
+        exts_last_position = int(sips_data[str(exts_last)][1])
+
+        # Add +1 to exts_new last position value
+        exts_new_last_position = exts_last_position + 1
+
+        # Copy old exts to exts_new
+        # We'll need before and after value of exts later
+        exts_new = exts.copy()
+
+        # Insert user's input extension to exts_new in sorted order
+        bisect.insort_left(exts_new, str(input_extension))
+        # If input extension less than currently last extension
+        if exts_last > input_extension:
+            # Find input_extension index number in exts_new list
+            input_extension_index = exts_new.index(str(input_extension))
+
+            # Next sip positions after inserted
+            next_positions = []
+
+            # Iterate from input_extension index to exts last index
+            for i in range(input_extension_index, len(exts)):
+                # Add sip position to next_positions list
+                next_positions.append(sips_data[exts[i]][1])
+
+            # Add to next_positions list, convert back to string
+            next_positions.append(str(exts_new_last_position))
+
+            # Create list of new input data, including new position
+            # Then add to sips_data
+            input_data = [str(input_extension),
+                          next_positions[0],
+                          input_label,
+                          input_context]
+
+            sips_data[str(input_extension)] = input_data
+
+            # Remove first index from next_positions
+            next_positions.pop(0)
+
+            n = 0  # set initial n value to 0
+
+            # Iterate from after input_extension index to exts_new last index
+            for i in range((input_extension_index + 1), len(exts_new)):
+                # Update all sips position after
+                sips_data[exts_new[i]][1] = next_positions[n]
+                n += 1  # n = n + 1
+
+        # Else if input extension greater than currently last extension
+        else:
+            # Create list of new input data, including new position
+            # Then add to sips_data
+            input_data = [str(input_extension),
+                          exts_new_last_position,
+                          input_label,
+                          input_context]
+
+            sips_data[str(input_extension)] = input_data
+
+        # Write data to config file
+        write_config(exts_new, sips_data)
+        print('Extension number {0} succesfully added.'
+              .format(str(input_extension)))
+
+    # Delete extension
+    elif sys.argv[1] == 'del':
+        print('delete')
+    # View extension
+    elif sys.argv[1] == 'view':
+        print('view')
+    # Not a valid action
     else:
-        print('Please enter a Label value. Try again ...')
+        print('Oops! That was not a valid actions. Exiting.')
 
-# get input for Context & some validation
-while True:
-    input_context = input('Context: ').strip()
-    # make sure input is not empty
-    if input_label:
-        break
-    else:
-        print('Please enter a Context value. Try again ...')
-
-# get last extension
-exts_last = int(exts[-1])
-
-# get sip last position value, convert to int
-exts_last_position = int(sips_data[str(exts_last)][1])
-
-# add +1 to exts_new last position value
-exts_new_last_position = exts_last_position + 1
-
-# copy old exts to exts_new
-# we need before and after exts later
-exts_new = exts.copy()
-
-# insert user's input extension to exts_new in sorted order
-bisect.insort_left(exts_new, str(input_extension))
-
-# if input extension less than currently last extension
-if exts_last > input_extension:
-    # find input_extension index number in exts_new list
-    input_extension_index = exts_new.index(str(input_extension))
-
-    # next sip positions after inserted
-    next_positions = []
-
-    # iterate from input_extension index to exts last index
-    for i in range(input_extension_index, len(exts)):
-        # add sip position to next_positions list
-        next_positions.append(sips_data[exts[i]][1])
-
-    # add to next_positions list, convert back to string
-    next_positions.append(str(exts_new_last_position))
-
-    # create list of new input data, including new position
-    # then add to sips_data
-    input_data = [str(input_extension),
-                  next_positions[0],
-                  input_label,
-                  input_context]
-
-    sips_data[str(input_extension)] = input_data
-
-    # remove first index from next_positions
-    next_positions.pop(0)
-
-    n = 0  # set initial n value to 0
-
-    # iterate from after input_extension index to exts_new last index
-    for i in range((input_extension_index + 1), len(exts_new)):
-        # update all sips position after
-        sips_data[exts_new[i]][1] = next_positions[n]
-        n += 1  # n = n + 1
-
-# else if input extension greater than currently last extension
+# List all sips
+elif len(sys.argv) == 2 and sys.argv[1] == 'list':
+    # TODO: list all
+    print('list')
+# No arguments/parameters given, exiting
 else:
-    # create list of new input data, including new position
-    # then add to sips_data
-    input_data = [str(input_extension),
-                  exts_new_last_position,
-                  input_label,
-                  input_context]
-
-    sips_data[str(input_extension)] = input_data
-
-for ext in exts_new:
-    print('[SIP/{0}]'.format(sips_data[ext][0]))
-    print('Position={0}'.format(sips_data[ext][1]))
-    print('Label=\"{0}\"'.format(sips_data[ext][2]))
-    print('Extension={0}'.format(sips_data[ext][0]))
-    print('Context={0}'.format(sips_data[ext][3]))
-    print('icon=1')
-    print('\n')
+    print('Please provide action or parameters. Exiting now.')
+    sys.exit()
